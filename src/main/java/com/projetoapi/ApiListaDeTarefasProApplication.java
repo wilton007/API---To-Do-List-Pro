fb6746c7c815be984930926681c1c7d893dab97f
anaceciliahope <anaceciliahope@gmail.com>
2025-04-08 20:43:52 -0300

    initial commit

---
+++ src/main/java/com/projetoapi/ApiListaDeTarefasProApplication.java
@@ -0,0 +1,13 @@
+package com.projetoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiListaDeTarefasProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiListaDeTarefasProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoapi/ApiListaDeTarefasProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiListaDeTarefasProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
